13ce4d7f17e1299406c7f8ba5374e5a36def0d18
mariyamjinana <mariyamjinana@gmail.com>
2024-11-05 16:08:34 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,104 @@
 import './App.css'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <>
-     <div className='bg-dark d-flex justify-content-center align-items-center'  style={{height:'100vh',width:'100%'}}>
-      <div className='bg-light p-5 rounded-2' style={{width:'500px'}}>
-        <h1>Simple Interest App</h1>
-        <p>Calculate your simple interest app</p>
-        <div className='bg-warning p-3 mt-4 d-flex justify-content-center align-items-center flex-column rounded' style={{height:'130px'}}>
-          <h1>₹ 100</h1>
-          <p>Total simple interest</p>
-        </div>
-        <div className="my-3 ">
-        <TextField id="outlined-basic" label="₹ Principal Amount" variant="outlined" className='w-100'/>
-        </div>
-        <div className="mb-3">
-        <TextField id="outlined-basic" label="Rate of interest (%)" variant="outlined" className='w-100' />
-        </div>
-        <div className="mb-3">
-        <TextField id="outlined-basic" label="Year" variant="outlined" className='w-100' />
-        </div>
-        <div className="mb-3 d-flex justify-content-between">
-        <Button variant="contained" style={{width:'190px'}} className='p-3' color='success'>Calculate</Button>
-        <Button variant="outlined" style={{width:'190px'}} className='p-3' color='success'>Reset</Button>
-        </div>
-      </div>
-     </div>
-    </>
-  )
+
+	const [Principle,setPrinciple] = useState("")
+	const [rate, setRate] = useState("")
+	const [year, setYear] = useState("")
+	const [isPrinciple,setIsPrinciple] = useState(true)
+	const [isRate,setIsRate] = useState(true)
+	const [isYear,setIsYear] = useState(true)
+	const [Interest,setInterest] = useState(0)
+
+	
+	const validate = (e) =>{
+		const {name,value} = e.target
+		console.log(name);
+		console.log(value);
+		
+		//match(regExp) - return array when values matches wit the regular expression else returns null 
+		// ^[0-9]$
+		if(!!value.match('^[0-9]*$')){
+			if(name == 'principle'){
+				setPrinciple(value)
+				setIsPrinciple(true)
+			}
+			else if(name == 'rate'){
+				setRate(value)
+				setIsRate(true)
+			}
+			else{
+				setYear(value)
+				setIsYear(true)
+			}
+		}
+		else{
+			if(name == 'principle'){
+				setPrinciple(value)
+				setIsPrinciple(false)
+			}
+			else if(name == 'rate'){
+				setRate(value)
+				setIsRate(false)
+			}
+			else{
+				setYear(value)
+				setIsYear(false)
+			}
+		}
+		}
+		
+
+	const handleReset = ()=>{
+		setPrinciple("")
+		setRate("")
+		setYear("")
+		setIsPrinciple(true)
+		setIsRate(true)
+		setIsYear(true)
+		setInterest(0)
+	}
+			
+	const calculate =()=>{
+		setInterest((Principle*rate*year)/100)
+	}
+	return (
+		<>
+			<div className='bg-black d-flex justify-content-center align-items-center' style={{ height: '100vh', width: '100%' }}>
+				<div className='bg-light p-5 rounded-2' style={{ width: '500px' }}>
+					<h1>Simple Interest App</h1>
+					<p>Calculate your simple interest easily</p>
+
+					<div className='bg-warning d-flex justify-content-center align-items-center p-3 rounded mt-4 flex-column' style={{ height: '150px' }}>
+						<h1>₹ {Interest}</h1>
+						<h5>Total simple interest</h5>
+					</div>
+					<div className='my-3'>
+						<TextField id="outlined-basic" value={Principle} className='w-100' label="₹ Principle amount" name='principle' variant="outlined" onChange={(e)=>validate(e)} />
+						{ isPrinciple == false && <p className='text-danger'>*Invalid Input</p>}
+
+					</div>
+					<div className="mb-3">
+						<TextField id="outlined-basic" className='w-100' value={rate} label="Rate of Interest (%)" name='rate' variant="outlined" onChange={(e)=>validate(e)} />
+						{ isRate == false && <p className='text-danger'>*Invalid Input</p>}
+
+					</div>
+					<div className="mb-3">
+						<TextField id="outlined-basic" className='w-100' value={year} label="Year" variant="outlined" name='year' onChange={(e)=>validate(e)} />
+						{ isYear == false &&<p className='text-danger'>*Invalid Input</p>}
+
+					</div>
+					<div className="mb-3 d-flex justify-content-between">
+					<Button variant="contained" style={{width:'190px'}} color='success' className='p-3' onClick={calculate}>Calculate</Button>
+					<Button variant="outlined" style={{width:'190px'}} className='p-3' onClick={handleReset}>Reset</Button>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default App
